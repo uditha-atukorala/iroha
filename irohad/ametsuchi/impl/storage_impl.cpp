e5759b10b0b8d8821941d6ca9e7dffbde187ea3c
kamilsa <kamilsa16@gmail.com>
2018-04-27 07:35:18 +0300

    Update comments
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- irohad/ametsuchi/impl/storage_impl.cpp
+++ irohad/ametsuchi/impl/storage_impl.cpp
@@ -183,7 +183,7 @@ DROP TABLE IF EXISTS index_by_id_height_asset;
 )";
 
       // erase db
-      log_->info("drop dp");
+      log_->info("drop db");
       pqxx::connection connection(postgres_options_.optionsString());
       pqxx::work txn(connection);
       txn.exec(drop);
@@ -205,6 +205,7 @@ DROP TABLE IF EXISTS index_by_id_height_asset;
           std::make_unique<pqxx::lazyconnection>(options_str_without_dbname);
       auto transaction =
           std::make_unique<pqxx::nontransaction>(*temp_connection);
+      // check if database dbname exists
       auto result = transaction->exec(
           "SELECT datname FROM pg_catalog.pg_database WHERE datname = "
           + transaction->quote(dbname));
@@ -254,11 +255,10 @@ DROP TABLE IF EXISTS index_by_id_height_asset;
                  [&block_store_dir](const PostgresOptions &options)
                  -> expected::Result<std::shared_ptr<StorageImpl>,
                                      std::string> {
-        auto dbname = options.getOption("dbname");
-        if (dbname) {
-          createDatabaseIfNotExist(dbname.value(),
+        options.getOption("dbname") | [&options](const auto &dbname) {
+          createDatabaseIfNotExist(dbname,
                                    options.optionsStringWithoutDbName());
-        }
+        };
 
         auto ctx_result =
             initConnections(block_store_dir, options.optionsString());
@@ -294,8 +294,8 @@ DROP TABLE IF EXISTS index_by_id_height_asset;
     }
 
     std::shared_ptr<WsvQuery> StorageImpl::getWsvQuery() const {
-      auto postgres_connection =
-          std::make_unique<pqxx::lazyconnection>(postgres_options_.optionsString());
+      auto postgres_connection = std::make_unique<pqxx::lazyconnection>(
+          postgres_options_.optionsString());
       try {
         postgres_connection->activate();
       } catch (const pqxx::broken_connection &e) {
