620c18bbffd15bd379f45a08512b2c6c7a04a9c6
rodionov12 <rodionov12@gmail.com>
2018-06-13 16:54:37 +0300

    Python bindings - Windows. Remove unused files
    Signed-off-by: rodionov12 <rodionov12@gmail.com>

--- python_bindings/build_templates.py
+++
@@ -1,7 +0,0 @@
-  import jinja2
-
-  templateLoader = jinja2.FileSystemLoader(searchpath="./files")
-  templateEnv = jinja2.Environment(loader=templateLoader)
-  TEMPLATE_FILE = "template.html"
-  template = templateEnv.get_template(TEMPLATE_FILE)
-  outputText = template.render()  # this is where to put args to the template renderer
--- python_bindings/files/__init__.py
+++
@@ -1,5 +0,0 @@
-from .iroha import *
-from .block_pb2 import *
-from .endpoint_pb2 import *
-from .queries_pb2 import *
-from .commands_pb2 import *
